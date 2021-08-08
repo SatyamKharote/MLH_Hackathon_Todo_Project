6fc7340ac0746ebad49ce8ede666ebaf5b691c26
Satyam Kharote <76524273+SatyamKharote@users.noreply.github.com>
2021-08-08 05:57:30 +0530

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Todo(models.Model):
-    email = models.EmailField(max_length=70,blank=True,unique=True)
     title = models.CharField(max_length=100)
     memo = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
